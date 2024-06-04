6c09f0ab6b4cc47bd323e65c87f59f9ccf5f161a
TwistedUmbrellaX <twistedumbrella@gmail.com>
2024-06-03 22:20:27 -0400

    Move licensed files to subdirectory

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -2,16 +2,18 @@ import { defineClientConfig } from "vuepress/client";
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
 import "vuepress-theme-hope/presets/bounce-icon.scss";
 
-import Changelog from "./components/Changelog.vue";
-import Contributors from "./components/Contributors.vue";
-import DownloadButton from "./components/DownloadButton.vue";
-import ReleaseDate from "./components/ReleaseDate.vue";
+import Changelog from "./mihon/components/Changelog.vue";
+import ChangelogsList from "./mihon/components/ChangelogsList.vue";
+import Contributors from "./mihon/components/Contributors.vue";
+import DownloadButton from "./mihon/components/DownloadButton.vue";
+import ReleaseDate from "./mihon/components/ReleaseDate.vue";
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {},
   setup() {},
   rootComponents: [
     "Changelog",
+    "ChangelogsList",
     "Contributors",
     "DownloadButton",
     "ReleaseDate"
---
+++ src/changelogs/index.md
@@ -0,0 +1,14 @@
+---
+title: Changelogs
+description: Changelogs of all Himitsu stable releases.
+lastUpdated: false
+editLink: false
+prev: false
+next: false
+---
+
+# Changelogs
+
+Changelogs of all Himitsu stable releases, which are also available [on GitHub](https://github.com/RepoDevil/Himitsu/releases). Beta releases can be seen [on GitHub](https://github.com/RepoDevil/TsubakiBuilder/releases).
+
+<ChangelogsList />
